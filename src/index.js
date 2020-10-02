a034e4ec38afa8970ea975c473cd1f0ddab427eb
Nevold <nevold@tut.by>
2020-10-02 23:25:49 +0300

    feat: modified functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = function min (array){
+   if(Array.isArray(array) && array.length >0){
+     let minNumber = array[0];
+     for (var i = 1; i < array.length; i++){
+       if (array[i] < minNumber){
+          minNumber = array[i];
+       }
+     }
+     return minNumber;
+   } else {
+     return 0;
+   }
+ }
+ 
+ 
+ exports.max = function max (array){
+   if(Array.isArray(array) && array.length >0){
+     let maxNumber = array[0];
+     for (var i = 1; i < array.length; i++){
+       if (array[i] > maxNumber){
+          maxNumber = array[i];
+       }
+     }
+     return maxNumber;
+   } else {
+     return 0;
+   }
+ }
+ 
+ 
+  exports.avg = function avg (array){
+   if(Array.isArray(array) && array.length >0){
+     let averageNumber=0;
+     for (var i = 0; i < array.length; i++){
+         averageNumber+=array[i];
+     }
+     return averageNumber / array.length;
+   } else {
+     return 0;
+   }
+ }
